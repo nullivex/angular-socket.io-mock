4f77e106e0ebd99a9de6c9db1516d1b51198643f
Mark Lawlor <mwlawlor@gmail.com>
2015-05-21 15:52:34 +1000

    Updated .receive to acknowledge any .emit events with a callback as the last parameter
    See: http://socket.io/docs/#sending-and-getting-data-(acknowledgements)

--- angular-socket.io-mock.spec.js
+++ angular-socket.io-mock.spec.js
@@ -2,17 +2,35 @@
 'use strict';
 
 describe('Angular Socket.io Mock',function(){
+  var socketFactory;
   beforeEach(module('btford.socket-io'))
-  it('should be able to listen on an event',inject(function(socketFactory){
+  beforeEach(inject(function(_socketFactory_){ socketFactory = _socketFactory_ }));
+  it('should be able to listen on an event', function(){
     expect(new socketFactory().on('test-event',function(){})).not.toBe(false)
-  }))
-  it('should be able to listen once event',inject(function(socketFactory){
-    expect(socketFactory.once('test-event',function(){})).not.toBe(false)
-  }))
-  it('should be able to emit an event',inject(function(socketFactory){
+  });
+  it('should be able to listen once event', function(){
+    expect(new socketFactory().once('test-event',function(){})).not.toBe(false)
+  });
+  it('should be able to emit an event', function(){
     expect(new socketFactory().emit('test-event',{})).not.toBe(false)
-  }))
-  it('should be able to receive an event',inject(function(socketFactory){
+  });
+  it('should be able to receive an event', function(){
     expect(new socketFactory().receive('test-event',{})).not.toBe(false)
-  }))
+  });
+  it('should be able to acknowledge an emited event only once',function(done){
+    var socket = new socketFactory();
+    var timesCalled = 0;
+
+    socket.emit('test-event',{}, function(resp){
+      expect(resp).not.toBe(false);
+      expect(++timesCalled).toEqual(1);
+    });
+
+    socket.receive('test-event', {});
+    socket.receive('test-event', {});
+
+    setTimeout(function() { // Wait to see if the event was acknowledged twice
+      done();
+    }, 100);
+  });
 })
